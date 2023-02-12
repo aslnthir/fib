1a6dedb245132c81219e67950a36d6ad4fffb5f1
Tahir Aslan <tahiraslan@Tahirs-Mac-mini.lan>
2023-02-13 00:25:29 +0100

    sist

---
+++ github.com/aslnthir/fib/go.mod
@@ -0,0 +1,3 @@
+module github.com/aslnthir/is105
+
+go 1.19
---
+++ github.com/aslnthir/fib/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"new/fibny"
+)
+
+func main() {
+
+	final := fibny.fibny(10)
+	fmt.Println("Final")
+
+}
